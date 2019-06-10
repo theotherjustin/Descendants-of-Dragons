7d313fac510300664f66d35c2db35bc018d62a31
Victor Wang <vgwang@ucsc.edu>
2019-06-09 23:04:45 -0700

    puzzle stage

---
+++ EternalGame/js/states/Level3.js
@@ -0,0 +1,63 @@
+var Level3 = function(game){
+	//define variables
+	var player;
+};
+Level3.prototype = {
+	preload: function(){
+		//load level
+		game.load.path = 'assets/img/';
+		game.load.tilemap('level3', 'level3Final.json', null, Phaser.Tilemap.TILED_JSON);
+	},
+	create:function(){
+		//background
+		castle = game.add.tileSprite(0,0,1281, 721, 'Castle');
+		//tilemap setup
+		this.map = game.add.tilemap('level3');
+		this.map.addTilesetImage('floors', 'tilesheetf');
+		this.map.setCollisionByExclusion([]);
+		this.mapLayer = this.map.createLayer('Tile Layer 1');
+
+		//prevent clipping
+		game.physics.arcade.TILE_BIAS = 32;
+
+		//add the block
+		block = game.add.sprite(150,400, 'key', 'block');
+		game.physics.enable(block,Phaser.Physics.ARCADE);
+		block.body.collideWorldBounds = true;
+		block.scale.setTo(1.75, 1.75);
+		block.body.gravity.y = 1000;
+		block.body.drag.setTo(1000, 0);
+		block.body.immovable = true;
+
+		//instruction sign
+		sign = game.add.sprite(0,620, 'key', 'sign');
+
+		//add the player
+		player = new Player(game, 20, 550, 2, 0);
+		game.add.existing(player);
+
+	},
+	update:function(){
+		//collision
+		game.physics.arcade.collide(player, this.mapLayer);
+		game.physics.arcade.collide(block, this.mapLayer);
+		game.physics.arcade.collide(player, block, this.blockColl, null, this);
+
+		//debug
+		game.debug.bodyInfo(player, 32, 32);
+		game.debug.body(player);
+
+	//next lvl
+	if(player.x > 1280) {
+		game.state.start('Level5');
+	}
+},
+blockColl:function(){
+		//make it so that only the ox moves the block
+		if(player.SpiritType == 3){
+			block.body.immovable = false;
+		}else{
+			block.body.immovable = true;
+		}
+	}
+};
