0e3cf82eead8e024711faf7579c49a9ab3cf30e9
Justin Lee <justindlee@comcast.net>
2019-06-09 23:25:35 -0700

    Update MainMenu.js

--- EternalGame/js/states/MainMenu.js
+++ EternalGame/js/states/MainMenu.js
@@ -58,7 +58,7 @@ MainMenu.prototype = {
 		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
 			//this.main.stop();
 			this.timer.stop();
-			game.state.start('Puzzle1');
+			game.state.start('Cut1');
 		}
 	}
 
